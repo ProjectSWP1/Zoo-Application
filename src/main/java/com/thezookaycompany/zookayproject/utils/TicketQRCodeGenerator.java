5f9cfe7b6292c4a7971b2e629a4516c284643d0f
NhanDz <lmnhn995@gmail.com>
2023-11-16 01:13:49 +0700

    update ticketQRCode

--- src/main/java/com/thezookaycompany/zookayproject/utils/TicketQRCodeGenerator.java
+++ src/main/java/com/thezookaycompany/zookayproject/utils/TicketQRCodeGenerator.java
@@ -1,5 +1,7 @@
 package com.thezookaycompany.zookayproject.utils;
 
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
@@ -7,6 +9,7 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.thezookaycompany.zookayproject.model.entity.Orders;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -15,6 +18,8 @@ import java.nio.file.Path;
 // For Ticket QR Generators after Payment Receipt Sent
 public class TicketQRCodeGenerator {
     private String url = "https://zookay-web.vercel.app";
+    private String azureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=cs110032002f9d9b454;AccountKey=Z64dIy9oQoDQLvHCWEJfWtaL5uBk5WkVOhguqSbw14+ol1NI3iz/4pApTiTJd1D1FxLtSluoD7Fc+AStTc7KjA==;EndpointSuffix=core.windows.net";
+    private String containerName = "qrcode";
 
     public void generateQRCodeImage(Orders orders, int width, int height, String filePath) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
@@ -22,16 +27,34 @@ public class TicketQRCodeGenerator {
 
         Path path = FileSystems.getDefault().getPath(filePath+orders.getOrderID()+"-QRCODE.png");
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-
-    }
-
-    public byte[] getQRCodeImage(Orders orders, int width, int height) throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(url+"/scan-ticket?orderID=&"+ orders.getOrderID() +"&email="+ orders.getEmail(), BarcodeFormat.QR_CODE, width, height);
+        BlobContainerClient containerClient = new BlobContainerClientBuilder()
+                .connectionString(azureStorageConnectionString)
+                .containerName(containerName)
+                .buildClient();
 
         ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
         MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
         byte[] pngData = pngOutputStream.toByteArray();
+
+        containerClient.getBlobClient(orders.getOrderID() + "-QRCODE.png").upload(new ByteArrayInputStream(pngData), pngData.length, true);
+    }
+
+    public byte[] getQRCodeImage(Orders orders, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(url + "/scan-ticket?orderID=" + orders.getOrderID() + "&email=" + orders.getEmail(), BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+
+        // Upload to Azure Storage
+        BlobContainerClient containerClient = new BlobContainerClientBuilder()
+                .connectionString(azureStorageConnectionString)
+                .containerName(containerName)
+                .buildClient();
+
+        containerClient.getBlobClient(orders.getOrderID() + "-QRCODE.png").upload(new ByteArrayInputStream(pngData), pngData.length, true);
+
         return pngData;
     }
 
